7f77c68d308abddfca7078b91d67da98ac1a0612
GabrielBittar <gabrielbittardomingues@gmail.com>
2020-03-29 01:11:46 -0400

    Comeca a implementar o modelo de cards de acordo com a proposta

--- resources/views/badge/index.blade.php
+++ resources/views/badge/index.blade.php
@@ -2,15 +2,22 @@
 
 @section('content')
 
+@include('snippets.fonts')
+
     <div class="row justify-content-center">
 
         <div class="col-sm-8 flex-column d-flex stretch-card">
             <div class="row">
                 @foreach($badges as $badge)
-                    <div class="col-lg-4 d-flex grid-margin stretch-card">
+                    <div class="col-lg-6 d-flex grid-margin stretch-card">
                         <div class="card sale-diffrence-border">
                             <div class="card-body">
-                                <h2 class="text-dark mb-2 font-weight-bold">{{$badge->name}}</h2>
+                                <div class="card-descricao">
+                                    <p class="card-campo-nome"><b>Nome:</b></p>
+                                    <h2 class="badge-nome mb-2 font-weight-bold">{{$badge->name}}</h2>
+                                    <!--TODO: adicionar o campo descricao nas badges no banco de dados para isto funcionar...-->
+                                    <p class="badge-descricao">{{$badge->descricao}}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
