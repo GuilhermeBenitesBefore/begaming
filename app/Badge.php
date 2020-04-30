7448d021fb0d8a175b1cd5d543d549f0f2c66c06
GabrielBittar <gabrielbittardomingues@gmail.com>
2020-04-29 22:06:34 -0400

    Cria migration com os niveis dos badges e constantes de cada badge

--- app/Badge.php
+++ app/Badge.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Badge extends Model
 {
+    const ADMIRACAO = 1;
+    const BEM_ESTAR = 2;
+    const CURSOS_PALESTRAS = 3;
+    const PALESTRAR = 4;
+    const DESEMPENHO_META = 5;
+    const TEMPO_EMPRESA = 6;
+    const COMPETICOES_INTERNAS = 7;
+    const CULTURA = 8;
+    const ATITUDE_EXTRAORDINARIA = 9;
+
     protected $fillable = ['name'];
 }
